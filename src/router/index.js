6872c1723047dbb43c7565c0976fcb2fd76cd426
yb <840823561@qq.com>
2019-11-06 16:27:37 +0800

    subject模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
-// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
-
 Vue.use(Router)
 
 /* Layout */
@@ -25,6 +22,7 @@ import Layout from '../views/layout/Layout'
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
   { path: '/404', component: () => import('@/views/404'), hidden: true },
+
 
   {
     path: '/',
@@ -214,14 +212,35 @@ export const constantRouterMap = [
     ]
   },
 
+  // {
+  //   path: 'my-homepage',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'http://maweitao.top',
+  //       meta: { title: '个人主页', icon: 'link' }
+  //     }
+  //   ]
+  // },
+
   {
-    path: 'my-homepage',
-    component: Layout,
-    children: [
+    path:'/common',
+    component:Layout,
+    meta:{title:'科目/专业/班级管理',icon:'info'},
+    children:[
       {
-        path: 'http://maweitao.top',
-        meta: { title: '个人主页', icon: 'link' }
-      }
+        path:'subject',
+        name:'subject',
+        meta:{title:'科目管理',icon:'info'},
+        component: ()=> import('@/views/common/Subject'),
+      },
+      {
+        path:'major',
+        name:'major',
+        meta:{title:'专业管理',icon:'info'},
+        component: ()=>import('@/views/common/Major')
+      },
+
     ]
   },
 
---
+++ src/views/common/Major.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="major">
+      this is major
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Major'
+  }
+</script>
+
+<style scoped>
+
+</style>
